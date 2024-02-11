b08a431e9ab6f0d24ac8445f426e78af83052173
muralikumar27 <muralikumar62345@gmail.com>
2024-02-11 12:54:49 +0530

    Frogot password : sending verification mail if user is not activated before using forgot password

--- src/main/java/com/murali/expensetracker/repository/UserVerificationRepository.java
+++ src/main/java/com/murali/expensetracker/repository/UserVerificationRepository.java
@@ -1,10 +1,15 @@
 package com.murali.expensetracker.repository;
 
+import com.murali.expensetracker.entity.User;
 import com.murali.expensetracker.entity.UserVerification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserVerificationRepository extends JpaRepository<UserVerification,Long> {
     UserVerification getByToken(String token);
+
+    Optional<UserVerification> getByUser(User user);
 }
--- src/main/java/com/murali/expensetracker/service/PasswordResetServiceImplementation.java
+++ src/main/java/com/murali/expensetracker/service/PasswordResetServiceImplementation.java
@@ -2,16 +2,17 @@ package com.murali.expensetracker.service;
 
 import com.murali.expensetracker.entity.PasswordToken;
 import com.murali.expensetracker.entity.User;
+import com.murali.expensetracker.entity.UserVerification;
 import com.murali.expensetracker.event.UserRegistrationEvent;
 import com.murali.expensetracker.exception.UserNotActivatedException;
 import com.murali.expensetracker.repository.PasswordTokenRepository;
 import com.murali.expensetracker.repository.UserRepository;
+import com.murali.expensetracker.repository.UserVerificationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.mail.MailException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
 import java.util.Optional;
 import java.util.UUID;
 
@@ -26,7 +27,7 @@ public class PasswordResetServiceImplementation implements PasswordResetService{
     @Autowired
     ApplicationEventPublisher applicationEventPublisher;
     @Autowired
-    UrlCreation urlCreation;
+    UserVerificationRepository userVerificationRepository;
 
     @Override
     public void resetPasswordTokenEmail(String email,String url) throws MailException, UserNotActivatedException {
@@ -38,7 +39,11 @@ public class PasswordResetServiceImplementation implements PasswordResetService{
 
         user = userFromRepo.get();
         if(!user.isEnabled()){
-            throw new UserNotActivatedException("Account not activated !");
+            Optional<UserVerification>userVerificationFromDB = userVerificationRepository.getByUser(user);
+            UserVerification userVerification = userVerificationFromDB.get();
+            userVerificationRepository.delete(userVerification);
+            applicationEventPublisher.publishEvent(new UserRegistrationEvent(user,url));
+            throw new UserNotActivatedException("Account not activated, check email for activation link !");
         }
 
         String token = UUID.randomUUID().toString();
